b7065e708a7de82946cdb8645a0e07403e1a38e6
Cody Henderson <cody@cthenderson.com>
2018-05-12 23:04:05 -0500

    Fix calendar event grabber

--- dynamic_powerpoint/calendar_utils.py
+++ dynamic_powerpoint/calendar_utils.py
@@ -27,7 +27,7 @@ class CalendarEvent:
             self.end = None
 
     def __str__(self):
-        return "{0} - {1} to {2}".format(self.name, self.start, self.end)
+        return "{0} - {1} to {2}: {3}".format(self.name, self.start, self.end, self.repeat_tag)
 
     @staticmethod
     def clean_datetime(val):
@@ -37,10 +37,11 @@ class CalendarEvent:
 class Calendar:
     def __init__(self, config):
         now = (config.get_date() - timedelta(days=1)).isoformat() + 'Z'
+        eight_weeks_later = (config.get_date() + timedelta(days=56)).isoformat() + 'Z'
         print(now)
 
-        url = "https://www.googleapis.com/calendar/v3/calendars/{0}/events?maxResults=40&singleEvents=true&timeMin={1}&key={2}"
-        url = url.format(config.get_calendar_id(), now, config.get_key())
+        url = "https://www.googleapis.com/calendar/v3/calendars/{0}/events?maxResults=100&singleEvents=true&timeMin={1}&timeMax={2}&key={3}"
+        url = url.format(config.get_calendar_id(), now, eight_weeks_later, config.get_key())
 
         print("requesting {0}".format(url))
         get_result = urllib.request.urlopen(url)
@@ -63,8 +64,8 @@ class Calendar:
 
     def get_events(self, start=None, end=None):
         events = [x for x in self.events if
-                  (start is None or start is not None and x.start > start) or
-                  (end is None or end is not None and x.end < end)]
+                  ((start is None or start is not None and x.start > start) and (start is None or start is not None and x.start < end)) or
+                  ((end is None or end is not None and x.end > start) and (end is None or end is not None and x.end < end))]
 
         events.sort(key=lambda event: event.start)
 
