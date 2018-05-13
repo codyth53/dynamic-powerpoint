02144c97fd08738ca122f315d692b12a0c3bd0ef
Cody Henderson <cody@cthenderson.com>
2018-05-12 23:13:01 -0500

    Move common functions

--- dynamic_powerpoint/powerpoint_generator/two_week_generator.py
+++ dynamic_powerpoint/powerpoint_generator/two_week_generator.py
@@ -6,15 +6,14 @@ from .powerpoint_generator import PowerpointGenerator
 from ..calendar_utils import CalendarEvent
 from datetime import datetime, timedelta
 from typing import List
-from .util import _hide_slide, _find_slide_with_name
+from .util import _hide_slide, _find_slide_with_name, _find_name_in_iterable, _find_sunday
 
 
 def generate_two_week(pg: PowerpointGenerator, slides: Slides):
-    start_date = find_sunday(pg.config.get_date())
+    start_date = _find_sunday(pg.config.get_date())
 
     events = pg.calendar.get_events(start=start_date, end=start_date + timedelta(days=14))
     unique_events = create_array_of_unique_events(events)
-
 
     groups_of_three = []
     while len(unique_events) > 0:
@@ -45,13 +44,13 @@ def generate_two_week_slide(slide: Slide,
                             all_events: List[CalendarEvent],
                             event_group: List[CalendarEvent],
                             start_date: datetime):
-    two_week_calendar_group = find_name_in_iterable(slide.shapes, 'dates')
+    two_week_calendar_group = _find_name_in_iterable(slide.shapes, 'dates')
     generate_calendar_dates(two_week_calendar_group.shapes, start_date)
 
-    event_marker_group = find_name_in_iterable(slide.shapes, 'event-markers')
+    event_marker_group = _find_name_in_iterable(slide.shapes, 'event-markers')
     generate_event_markers(event_marker_group.shapes, all_events, start_date)
 
-    event_blocks_group = find_name_in_iterable(slide.shapes, 'event-blocks')
+    event_blocks_group = _find_name_in_iterable(slide.shapes, 'event-blocks')
     generate_event_detail_boxes(event_blocks_group.shapes, event_group)
 
 
@@ -74,40 +73,25 @@ def generate_event_markers(group: GroupShapes, events: List[CalendarEvent], star
         this_day_events = [x for x in events if x.start >= this_day and x.end <= this_day + timedelta(days=1)]
         print("Day {0} has {1} events".format(this_day, len(this_day_events)))
         if len(this_day_events) < 2:
-            second_marker = find_name_in_iterable(event_group.shapes, 'event2')
+            second_marker = _find_name_in_iterable(event_group.shapes, 'event2')
             ele = second_marker.element
             ele.getparent().remove(ele)
         if len(this_day_events) < 1:
-            first_marker = find_name_in_iterable(event_group.shapes, 'event1')
+            first_marker = _find_name_in_iterable(event_group.shapes, 'event1')
             ele = first_marker.element
             ele.getparent().remove(ele)
 
 
 def generate_event_detail_boxes(group: GroupShapes, events: List[CalendarEvent]):
     for i in range(0, 3):
-        event_block = find_name_in_iterable(group, 'event-' + str(i+1))
+        event_block = _find_name_in_iterable(group, 'event-' + str(i+1))
         if len(events) > i:
             # handle block color
-            textbox = find_name_in_iterable(event_block.shapes, 'text')
+            textbox = _find_name_in_iterable(event_block.shapes, 'text')
             textbox.text = events[i].name
         else:
             ele = event_block.element
             ele.getparent().remove(ele)
-
-
-def find_sunday(date: datetime) -> datetime:
-    if date.weekday() == 6:
-        return date
-    else:
-        return date - timedelta(days=date.isoweekday())
-
-
-def find_name_in_iterable(iterable, name):
-    target = [x for x in iterable if x.name == name]
-    if len(target) == 0:
-        return None
-    else:
-        return target[0]
 
 
 def create_array_of_unique_events(events: List[CalendarEvent]) -> List[CalendarEvent]:
--- dynamic_powerpoint/powerpoint_generator/util.py
+++ dynamic_powerpoint/powerpoint_generator/util.py
@@ -1,4 +1,5 @@
 from pptx.slide import Slide, Slides
+from datetime import datetime, timedelta
 
 
 def _hide_slide(slide: Slide):
@@ -11,3 +12,18 @@ def _find_slide_with_name(slides: Slides, name) -> Slide:
             if name in slide.notes_slide.notes_text_frame.text:
                 return slide
     return None
+
+
+def _find_sunday(date: datetime) -> datetime:
+    if date.weekday() == 6:
+        return date
+    else:
+        return date - timedelta(days=date.isoweekday())
+
+
+def _find_name_in_iterable(iterable, name):
+    target = [x for x in iterable if x.name == name]
+    if len(target) == 0:
+        return None
+    else:
+        return target[0]
